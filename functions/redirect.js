77e526476c712c6de718668d48560a736d424abc
Sumit <72924107+sumit-ptdr@users.noreply.github.com>
2023-08-04 18:42:28 +0530

    added support for shortlinks

---
+++ functions/redirect.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require("mongodb");
+
+exports.handler = async function(event, context) {
+  const { path } = event;
+  const shortlinkId = path.split("/")[2];
+
+  if (!shortlinkId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid shortlink" }),
+    };
+  }
+
+  try {
+    // Replace 'your-mongodb-connection-string' with your actual MongoDB connection string
+    const uri = process.env.MONGODB_CONNECTION_STRING;
+    const client = new MongoClient(uri);
+
+    await client.connect();
+
+    const database = client.db("linkdb");
+    const collection = database.collection("linkdict");
+
+    const result = await collection.findOne({ shortlink_id: shortlinkId });
+
+    if (!result) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Shortlink not found" }),
+      };
+    }
+
+    client.close();
+
+    return {
+      statusCode: 301,
+      headers: {
+        Location: result.original_link,
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal server error" }),
+    };
+  }
+};
